c7833447266a0034f2edb537295f5f456902fe5c
JorgeR <jlramoss@yahoo.com.mx>
2023-05-14 13:01:12 -0500

    added django app

--- PDF_ReaderApp/pdf_reader/tasks/views.py
+++ PDF_ReaderApp/pdf_reader/tasks/views.py
@@ -1,19 +1,29 @@
-from django.shortcuts import render
 from django import forms
+from django.shortcuts import render
 
 # Create your views here.
 tasks=["foo", "var","baz"]
 
 class newTaskForm(forms.Form):
     task=forms.CharField(label="New Task")
-    priority=forms.NumberInput()
-
+    priority= forms.IntegerField(min_value=1,max_value=10,label="priority")
+    
+    
 def index(request):
     return render(request, "tasks/index.html",{
         "tasks" :tasks
     })
 
 def add(request):
+    if request.method=="POST":
+        form =newTaskForm(request.POST)
+        if form.is_valid():
+            task=form.cleaned_data["task"]
+            tasks.append(task)
+        else:
+            return render(request,"tasks/add.html",{
+                "form":form
+            })
     return render(request, "tasks/add.html",{
        
         "form" : newTaskForm()
